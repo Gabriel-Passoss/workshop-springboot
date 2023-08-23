4564253e67495f7a8658c540d82ba861b6b55428
Gabriel dos Passos <kooujinproductions@gmail.com>
2023-08-23 18:39:55 -0300

    Added find by id method in UserService and GET route to it

--- src/main/java/com/kooujin/course/resources/UserResource.java
+++ src/main/java/com/kooujin/course/resources/UserResource.java
@@ -4,6 +4,7 @@ import com.kooujin.course.entities.User;
 import com.kooujin.course.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,11 @@ public class UserResource {
     public ResponseEntity<List<User>> findall() {
         List<User> userList = userService.findAll();
         return ResponseEntity.ok().body(userList);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Object> findById(@PathVariable Long id) {
+        var user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
--- src/main/java/com/kooujin/course/services/UserService.java
+++ src/main/java/com/kooujin/course/services/UserService.java
@@ -5,6 +5,7 @@ import com.kooujin.course.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public Object findById(Long id) {
+        Optional<User> user =  userRepository.findById(id);
+        return user.get();
     }
 }

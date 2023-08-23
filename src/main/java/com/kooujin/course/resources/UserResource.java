4df4081fc297e4d94067c9fc279b08604a7d4bee
Gabriel dos Passos <kooujinproductions@gmail.com>
2023-08-23 19:05:41 -0300

    Fix findall to findAll

--- src/main/java/com/kooujin/course/resources/UserResource.java
+++ src/main/java/com/kooujin/course/resources/UserResource.java
@@ -20,7 +20,7 @@ public class UserResource {
     }
 
     @GetMapping
-    public ResponseEntity<List<User>> findall() {
+    public ResponseEntity<List<User>> findAll() {
         List<User> userList = userService.findAll();
         return ResponseEntity.ok().body(userList);
     }

fc6728d5286c8c00ebfe43ba9182c662bb7f1c20
Gabriel dos Passos <kooujinproductions@gmail.com>
2023-08-23 15:01:01 -0300

    Added UserService and refactor UserRepository and UserResource

--- src/main/java/com/kooujin/course/resources/UserResource.java
+++ src/main/java/com/kooujin/course/resources/UserResource.java
@@ -1,17 +1,26 @@
 package com.kooujin.course.resources;
 
 import com.kooujin.course.entities.User;
+import com.kooujin.course.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    private final UserService userService;
+
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping
-    public ResponseEntity<User> findall() {
-        User user = new User(1L, "Gabriel", "gabriel@icloud.com", "489999", "123");
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<List<User>> findall() {
+        List<User> userList = userService.findAll();
+        return ResponseEntity.ok().body(userList);
     }
 }
---
+++ src/main/java/com/kooujin/course/services/UserService.java
@@ -0,0 +1,20 @@
+package com.kooujin.course.services;
+
+import com.kooujin.course.entities.User;
+import com.kooujin.course.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
